beac2f8265077e56e44cc7c152bc09dc86844283
khadjafatima409 <khadijafatima530@gmail.com>
2026-01-13 01:49:44 +0500

    link script file , add grade calculator

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ if (a > b) {
 } else {
   console.log(false);
 }
+
+// 2. Grade Calculator (If–Else)
+// Take a score (0–100) as input and print grade:
+// A / B / C / D / F based on the score.
+
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("A");
+} else if (marks >= 80) {
+  console.log("B");
+} else if (marks >= 70) {
+  console.log("C");
+} else if (marks >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
