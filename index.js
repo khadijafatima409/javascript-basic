49d471e002dfd49d5814fecf662f28437f710be1
khadjafatima409 <khadijafatima530@gmail.com>
2026-01-13 01:45:20 +0500

    Add program to compare two numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1. Compare Two Numbers (Operators + Conditions)
+//  Write a program that takes two numbers and logs true if the first number is
+// greater than the second, otherwise false. write simpel code in js.
+
+let a = 5;
+let b = 3;
+
+if (a > b) {
+  console.log(true);
+} else {
+  console.log(false);
+}
